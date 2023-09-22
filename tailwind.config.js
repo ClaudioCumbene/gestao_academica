9a202796403e91344a8c569cc3647e113197e746
Isa Neide Sitoe <issitoe941@gmail.com>
2023-09-22 11:27:59 -0800

    adicionando conteudo a pagina inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./assets/**/*.{html,jsx}",
+"./index.html",
+],
   theme: {
     extend: {},
   },
